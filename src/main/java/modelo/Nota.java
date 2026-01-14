2460eecf6c5a7a46d082de9dcf3308a1b3dcd553
joão henrique <joaolgg2estudos@gmail.com>
2026-01-14 07:47:25 -0300

    novo atributo data ne entidade Nota

--- src/main/java/modelo/Nota.java
+++ src/main/java/modelo/Nota.java
@@ -1,11 +1,15 @@
 package modelo;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -17,6 +21,10 @@ public class Nota {
 	
 	@NotNull
 	private Double nota;
+	
+	private String descricao;
+
+	private LocalDate data;
 	
 	@ManyToOne	
 	private Curso curso;
@@ -55,4 +63,22 @@ public class Nota {
 	public void setNota(Double nota) {
 		this.nota = nota;
 	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
 }
